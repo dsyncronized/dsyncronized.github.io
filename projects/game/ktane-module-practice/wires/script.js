a390969bb4f37b58904c02aa64c4404b1e035e10
dsyncronized <randomme1906@hotmail.com>
2026-09-05 19:55:22 +0700

    full wires module done

--- projects/game/ktane-module-practice/wires/script.js
+++ projects/game/ktane-module-practice/wires/script.js
@@ -55,16 +55,35 @@ function renderBackground() {
     }
 }
 
+function generateString(length) {
+    const characters = "abcdefghijklmnpqrstuvwxz0123456789";
+    let result;
+
+    while (true) {
+        result = "";
+
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+
+        if (/[a-z]/.test(result) && /[0-9]/.test(result)) {
+            return result;
+        }
+    }
+}
+
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 function initializeGame() {
-//    const wireAmount = getRndInteger(3, 6);
-    const wireAmount = 3
+    const wireAmount = getRndInteger(3, 6);
     let wirePosition;
     const wireData = []
     const availablePositions = [0, 1, 2, 3, 4, 5]
+    let serialNumber = generateString(6)
+    document.getElementById("serial-number").innerHTML = serialNumber;
+    let lastNumber = serialNumber.match(/\d(?=[^\d]*$)/)[0];
 
     for (let i = 0; i < wireAmount; i++) {
         while (true) {
@@ -80,12 +99,13 @@ function initializeGame() {
     }
 
     return {
+        lastNumber: lastNumber,
         wireData: wireData,
         wireAmount: wireAmount
     };
 }
 
-const wires = initializeGame()
+let state = initializeGame()
 
 function renderGame() {
     const wireColors = {
@@ -96,9 +116,9 @@ function renderGame() {
         5: "#22222d"
     };
 
-    for (let i = 0; i < wires.wireData.length; i++) {
-        let wireColor = wires.wireData[i][1]
-        let wirePosition = wires.wireData[i][0]
+    for (let i = 0; i < state.wireData.length; i++) {
+        let wireColor = state.wireData[i][1]
+        let wirePosition = state.wireData[i][0]
 
         ctx.fillStyle = wireColors[wireColor];
         ctx.fillRect(
@@ -111,7 +131,15 @@ function renderGame() {
 }
 
 function getWiresInOrder() {
-    return [...wires.wireData].sort((a, b) => a[0] - b[0]);
+    return [...state.wireData].sort((a, b) => a[0] - b[0]);
+}
+
+function isSerialNumberOdd() {
+    if (state.lastNumber % 2 == 0) {
+        return false
+    } else {
+        return true
+    }
 }
 
 function getWireAtOrder(order) {
@@ -143,10 +171,10 @@ function getLastWireOfColor(color) {
 //      }
 //  }
 // }
-// 1 = red, 2 = blue, 3 = yellow, 4 = white, 5 = black
 
+// 1 = red, 2 = blue, 3 = yellow, 4 = white, 5 = black
 function validateWire(wireIndex) {
-    if (wires.wireAmount == 3) {
+    if (state.wireAmount == 3) {
 
         if (getColorCount(1) == 0) {
             if (wireIndex == 1) {
@@ -155,7 +183,6 @@ function validateWire(wireIndex) {
                 redFlash()
                 console.log(wireIndex)
             }
-
         } else if (getWireAtOrder(2)[1] == 4) {
             if (wireIndex == 2) {
                 greenFlash()
@@ -180,14 +207,98 @@ function validateWire(wireIndex) {
         }
     }
 
-    if (wires.wireAmount == 4) {
-
+    if (state.wireAmount == 4) {
+        if (getColorCount(1) > 1 && isSerialNumberOdd()) {
+            if (wireIndex == getLastWireOfColor(1)) {
+                greenFlash()
+            } else {
+                redFlash()
+            }
+        } else if (getWireAtOrder(3)[1] == 3 && getColorCount(1) == 0) {
+            if (wireIndex == 0) {
+                greenFlash()
+            } else {
+                redFlash()
+            }
+        } else if (getColorCount(2) == 1) {
+            if (wireIndex == 0) {
+                greenFlash()
+            } else {
+                redFlash()
+            }
+        } else if (getColorCount(3) > 1) {
+            if (wireIndex == 3) {
+                greenFlash()
+            } else {
+            redFlash()
+            }
+        } else {
+            if (wireIndex == 1) {
+                greenFlash()
+            } else {
+                redFlash()
+            }
+        }
     }
 
-    if (wires.wireAmount == 5) {
+    if (state.wireAmount == 5) {
+        if (getWireAtOrder(4)[1] == 5 && isSerialNumberOdd()) {
+            if (wireIndex == 3) {
+                greenFlash()
+            } else {
+                redFlash()
+            }
+        } else if (getColorCount(1) == 1 && getColorCount(3) > 1) {
+            if (wireIndex == 0) {
+                greenFlash()
+            } else {
+                redFlash()
+            }
+        } else if (getColorCount(5) == 0) {
+            if (wireIndex == 1) {
+                greenFlash()
+            } else {
+                redFlash()
+            }
+        } else {
+            if (wireIndex == 0) {
+                greenFlash()
+            } else {
+                redFlash()
+            }
+        }
+    }
 
+    if (state.wireAmount == 6) {
+        if (getColorCount(3) ==  0 && isSerialNumberOdd()) {
+            if (wireIndex == 2) {
+                greenFlash()
+            } else {
+                redFlash()
+            }
+        } else if (getColorCount(3) == 1 && getColorCount(4) > 1) {
+            if (wireIndex == 3) {
+                greenFlash()
+            } else {
+                redFlash()
+            }
+        } else if (getColorCount(1) == 0) {
+            if (wireIndex == 5) {
+                greenFlash()
+            } else {
+                redFlash()
+            }
+        } else {
+            if (wireIndex == 3) {
+                greenFlash()
+            }
+            else {
+                redFlash()
+            }
+        }
     }
 }
+// 1 = red, 2 = blue, 3 = yellow, 4 = white, 5 = black
 
 function clickableArea(mouseX, mouseY) {
     const wireX = canvas.width * 0.1075;
@@ -208,22 +319,47 @@ function clickableArea(mouseX, mouseY) {
     }
 }
 
-function greenFlash() {
-    ctx.fillStyle = "rgba(0, 255, 0, 0.3)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+async function greenFlash() {
+    const newState = initializeGame();
 
-    setTimeout(() => {
-        renderGame();
-    }, 150);
+    await fadeFlash("rgba(143, 197, 135, ");
+
+    state = newState;
+    renderBackground();
+    renderGame();
 }
 
-function redFlash() {
-    ctx.fillStyle = "rgba(255, 0, 0, 0.3)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+async function redFlash() {
+    const newState = initializeGame();
 
-    setTimeout(() => {
-        renderGame();
-    }, 150);
+    await fadeFlash("rgba(217, 94, 89, ");
+
+    state = newState;
+    renderBackground();
+    renderGame();
+}
+
+function fadeFlash(color) {
+    return new Promise(resolve => {
+        let opacity = 0.9;
+
+        function fade() {
+            renderBackground();
+            renderGame();
+
+            if (opacity > 0) {
+                ctx.fillStyle = color + opacity + ")";
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+                opacity -= 0.02;
+                requestAnimationFrame(fade);
+            } else {
+                resolve();
+            }
+        }
+
+        fade();
+    });
 }
 
 renderBackground()
